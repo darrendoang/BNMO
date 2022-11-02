97c5b4e61c383d4414dc5e830e8a714c864a4cf3
darrendoang <16521063@mahasiswa.itb.ac.id>
2022-11-02 23:36:32 +0700

    Penambahan score pada rng.c

--- Tugas-Besar-1/src/GAME/rng.c
+++ Tugas-Besar-1/src/GAME/rng.c
@@ -2,22 +2,25 @@
 #include<stdlib.h>
 #include<time.h>
 int main() {
-    int i;
+    int i , score= 10;
     printf("RNG Telah dimulai. Uji keberuntungan Anda dengan menebak X\n");
     srand(time(0));
     int r= rand();
     int flag = 0;
-    while (flag == 0)
+    
+    while (flag == 0 && score > 0)
     {
         printf("Tebakan: ");
         scanf("%d" , &i);
         if (i > r)
         {
             printf("Lebih kecil\n");
+            score = score -1;
         }
         else if (i < r)
         {
             printf("Lebih besar\n");
+            score = score -1;
         }
         else
         {
@@ -26,10 +29,14 @@ int main() {
         
         
     }
-    if (flag==1)
+    if (flag==1 && score > 0)
     {
         printf("Ya, X adalah %i" , r);
     }
-    
+    else if(score == 0)
+    {
+        printf("Kesempatan menebak Anda habis.");
+    }
+
     return 0;
 }
