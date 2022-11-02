dc8e32d4b7bb7da949f00ac3efe81f1dc9685b4a
haidar2003 <muhammadrafihaidar03@gmail.com>
2022-11-02 23:43:30 +0700

    Revisi Diner Dash

---
+++ Tugas-Besar-1/src/ADT/ddash_copy.h
@@ -0,0 +1,38 @@
+#include "boolean.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define IDX_UNDEF -1
+#define CAPACITY 100
+#define Nil -999
+
+typedef struct {
+	int number;
+	int time;
+	int duration;
+    int price;
+} order;
+
+typedef struct {
+	order buffer[CAPACITY]; 
+	int idxHead;
+	int idxTail;
+} orderQueue;
+
+#define IDX_HEAD(q) (q).idxHead
+#define IDX_TAIL(q) (q).idxTail
+#define     HEAD(q) (q).buffer[(q).idxHead]
+#define     TAIL(q) (q).buffer[(q).idxTail]
+
+void CreateQueue(orderQueue *q);
+
+boolean isEmpty(orderQueue q);
+
+int length(orderQueue q);
+
+void enqueue(orderQueue *q, int num);
+
+void dequeue(orderQueue *q, int *saldo);
+
+void dinnner_dash();
