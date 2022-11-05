887768faf69a18d2e770ff6714b5f148810a6351
audarcy <110438375+audarcy@users.noreply.github.com>
2022-11-05 13:36:29 +0700

    string comparison

---
+++ Tugas-Besar-1/src/ADT/stringcomp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "stringcomp.h"
+
+boolean str_comp(char str1[], char str2[]){
+    
+    int i=0;
+
+    while(str1[i] == str2[i]){
+        if( str1[i] == '\0' || str2[i] == '\0' ) 
+            break;
+        i++;
+    }
+
+    if( str1[i] == '\0' &&  str2[i] == '\0' )    
+        return true;    
+    else    
+        return false; 
+}
