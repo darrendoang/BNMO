b40294582c2fbb543067e65a1aadd6e20ee9d454
haidar2003 <muhammadrafihaidar03@gmail.com>
2022-11-08 20:29:32 +0700

    diner dash revisi terakhir

--- Tugas-Besar-1/src/main.c
+++ Tugas-Besar-1/src/main.c
@@ -17,67 +17,62 @@ int main(){
     printf("ENTER COMMAND: ");
 
     // Inisiasi input
-    Word command;
-    Word arg;
+    char *input;
+    char *command;
+    char *arg;
 
     // Meminta input
-    STARTWORD();   
-    SalinWord(currentWord, &command);
-    ADVWORD();
-    if (!EndWord){
-        SalinWord(currentWord, &arg);
-    }
+    input = READINPUT();
+    command = KataPertama(input);
+    arg = KataKedua(input);
     
     // Loop utama
     // (str_comp(command.TabWord, quit))||(str_comp(command.TabWord, help))
     boolean over = false;
     while (!over){
-        if (str_comp(command.TabWord, "START")){
+        if (str_comp(command, "START")){
             STARTGAME(&game);
         }
-        else if (str_comp(command.TabWord, "LOAD")){
-            LOAD(&game, arg.TabWord);
+        else if (str_comp(command, "LOAD")){
+            LOAD(&game, arg);
         }
-        else if (str_comp(command.TabWord, "SAVE")){
-            SAVE(game, arg.TabWord);
+        else if (str_comp(command, "SAVE")){
+            SAVE(game, arg);
         }
-        else if (str_comp(command.TabWord, "CREATEGAME")){
+        else if (str_comp(command, "CREATEGAME")){
             CREATEGAME(&game);
         }
-        else if (str_comp(command.TabWord, "LISTGAME")){
+        else if (str_comp(command, "LISTGAME")){
             LISTGAME(&game);
         }
-        else if (str_comp(command.TabWord, "DELETEGAME")){
-            DELETEGAME(&game);
+        else if (str_comp(command, "DELETEGAME")){
+            DELETEGAME(&game, antrian);
         }
-        else if (str_comp(command.TabWord, "QUEUEGAME")){
+        else if (str_comp(command, "QUEUEGAME")){
             QUEUEGAME(&antrian, game);
         }
-        else if (str_comp(command.TabWord, "PLAYGAME")){
+        else if (str_comp(command, "PLAYGAME")){
             PLAYGAME(&antrian, game);
         }
-        else if (str_comp(command.TabWord, "SKIPGAME")){
+        else if (str_comp(command, "SKIPGAME")){
             SKIPGAME();
         }
-        else if (str_comp(command.TabWord, "QUIT")){
+        else if (str_comp(command, "QUIT")){
             QUIT();
         }
-        else if (str_comp(command.TabWord, "HELP")){
+        else if (str_comp(command, "HELP")){
             HELP();
         }
         else{
-            printf("error\n");
+            printf("input error\n");
         }
 
         // Apabila belum keluar dari game
         if (!over){
             // Meminta input
-            STARTWORD();   
-            SalinWord(currentWord, &command);
-            ADVWORD();
-            if (!EndWord){
-                SalinWord(currentWord, &arg);
-            } 
+            input = READINPUT();
+            command = KataPertama(input);
+            arg = KataKedua(input);
         }
     }
     return 0;
