652d12fdff8aa5f5fbd051c47571b169d17a2a91
darrendoang <16521063@mahasiswa.itb.ac.id>
2022-11-10 00:33:30 +0700

    delete quit,skipgame

--- Tugas-Besar-1/src/ADT/quit.h
+++
@@ -1,7 +0,0 @@
-// Keluar dari Program
-#include <stdio.h>
-#include <stdlib.h>
-#include "mesinkata.h"
-#include "queue.h"
-
-void QUIT(Queue *Q){
--- Tugas-Besar-1/src/ADT/skipgame.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "queue.h"
-#include "mesinkata.h"
- 
-void SKIPGAME(Queue *antriangame, int n){
