8014e2c9fd4cf8b7da109d7645a951ae8984a4bf
darrendoang <16521063@mahasiswa.itb.ac.id>
2022-11-11 01:19:23 +0700

    Update driver_mesinload.c

--- Tugas-Besar-1/src/ADT/driver_mesinload.c
+++ Tugas-Besar-1/src/ADT/driver_mesinload.c
@@ -1,3 +1,22 @@
 #include "mesinload.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+int main (){  
+    char *filename = "../data/savefile.txt";
+    StartLOAD(filename);
+    int n = currentChar - '0'; 
+    printf("%s" , n);
+    ADVWORDLOAD();
+    for (int i = 0; i < n; i++) {
+        ADVWORDLOAD();
+        char *namagame;
+        namagame = (char *)malloc(currentWord.Length * sizeof(char));
+        for(int j = 0; j < currentWord.Length; j++)
+        {
+            *(namagame + j) = currentWord.TabWord[j];
+        }
+        *(namagame + currentWord.Length) = '\0'; //penanda akhir string
+        printf ("%s", namagame); 
+    }
+}
