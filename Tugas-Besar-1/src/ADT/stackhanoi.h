3434f1b7aaddd25111699dd16d0bd4dc00478cf7
darrendoang <16521063@mahasiswa.itb.ac.id>
2022-11-26 18:41:55 +0700

    Update stackhanoi.h

--- Tugas-Besar-1/src/ADT/stackhanoi.h
+++ Tugas-Besar-1/src/ADT/stackhanoi.h
@@ -20,7 +20,7 @@ typedef struct
 {
     stackinfotype T[MaxEl]; /* tabel penyimpan elemen */
     address TOP;            /* alamat TOP: elemen puncak */
-} Stackhanoi;
+} HanoiStack;
 /* Definisi stack S kosong : S.TOP = Nil */
 /* Elemen yang dipakai menyimpan nilai Stack T[0]..T[MaxEl-1] */
 /* Jika S adalah Stack maka akses elemen : */
@@ -33,31 +33,31 @@ typedef struct
 
 /* ************ Prototype ************ */
 /* *** Konstruktor/Kreator *** */
-void CreateEmptyStack(Stackhanoi *S);
+void CreateEmptyStack(HanoiStack *S);
 /* I.S. sembarang; */
 /* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
 /* jadi indeksnya antara 0.. MaxEl */
 /* Ciri stack kosong : TOP bernilai Nil */
 
 /* ************ Predikat Untuk test keadaan KOLEKSI ************ */
-boolean IsStackEmpty(Stackhanoi S);
+boolean IsStackEmpty(HanoiStack S);
 /* Mengirim true jika Stack kosong: lihat definisi di atas */
-boolean IsStackFull(Stackhanoi S);
+boolean IsStackFull(HanoiStack S);
 /* Mengirim true jika tabel penampung nilai elemen stack penuh */
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void Push(Stackhanoi *S, stackinfotype X);
+void Push(HanoiStack *S, stackinfotype X);
 /* Menambahkan X sebagai elemen Stack S. */
 /* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
 /* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
 
 /* ************ Menghapus sebuah elemen Stack ************ */
-void Pop(Stackhanoi *S, stackinfotype *X);
+void Pop(HanoiStack *S, stackinfotype *X);
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
 /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
-boolean IsCloneStack(Stackhanoi S1, Stackhanoi S2);
+boolean IsCloneStack(HanoiStack S1, HanoiStack S2);
 /* Mengirimkan true jika stack yang dibandingkan sama*/
 
-int top_val(Stackhanoi S);
+int top_val(HanoiStack S);
 #endif
