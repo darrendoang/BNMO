32b42b6b8548b0f75d7ee14fa912a3b1a126ddf6
audarcy <110438375+audarcy@users.noreply.github.com>
2022-12-01 12:10:10 +0700

    hartakarun

---
+++ Tugas-Besar-1/src/GAME/hartakarun.c
@@ -0,0 +1,166 @@
+#include "hartakarun.h"
+
+int peti(BinTree x)
+{
+    if (isOneElmt(x)){
+        return ROOT(x);
+    }
+    else{
+        int left;
+        int right;
+        if(!isTreeEmpty(x)){
+            left = ROOT(x) + peti(LEFT(x));
+            right = ROOT(x) + peti(RIGHT(x));
+
+            if (left >= right){
+                return left;
+            }
+            else if (right > left){
+                return right;
+            }
+
+        }
+    }
+}
+
+// READINPUT() buat minta input dari keyboard
+// jadi tinggal 
+// char  *x ;
+// x = READINPUT() ;
+// nanti inputnya ke simpen di x tapi masih dlm string
+// kalo mau dirubah ke INT x nya tinggal buat variabel baru
+// terus pake fungsi laen kyk gini
+// int x_int;
+// x_int = StrToInt_input(x_int , str_len(x_int));
+
+// ini x_int udh jdi int
+
+int PathChosen(BinTree x)
+{
+    boolean validInput = false;
+    int hasil = ROOT(x);
+
+    while(!isOneElmt(x)){
+        char jalur;
+
+        while (!validInput){
+
+            printf("Pilih jalur yang ingin diambil ('L'/'R') : ");
+            char  *jalur ;
+            jalur = READINPUT() ;
+            
+            if (jalur == 'L'){
+                printf("Jalur kiri telah dipilih.\n");
+                hasil += ROOT(LEFT(x));
+                x=LEFT(x);
+                validInput = true;  
+            }
+
+            else if (jalur == 'R'){
+                printf("Jalur kanan telah dipilih.\n");
+                hasil += ROOT(RIGHT(x));
+                x=RIGHT(x);
+                validInput = true;
+            }
+
+            else{
+                printf("Masukkan invalid.\n");
+            }
+
+        }
+        validInput = false;
+    }
+    return hasil;
+}
+
+
+
+BinTree TimesTwo()
+{
+    int n;
+
+    //input jumlah daun
+    printf("Banyak peti harta karun yang dapat digali : \n");
+    char  *jumlah ;
+    jumlah = READINPUT() ;
+    n= StrToInt_input(jumlah , str_len(jumlah));
+
+
+    //jumlah daun harus lebih dari 0
+    if(n <= 0){
+        printf("Masukkan minimal 1 peti harta karun !\n");
+        return 0;
+    }
+
+
+    int temp = n;
+    while(temp%2 == 0){
+        temp /= 2;
+    }
+    if(temp != 1){
+        printf("Jumlah masukan tidak sesuai \n");
+        return 0;
+    }
+
+    int batas = (n*2)-1;
+    BinTree ar[batas];
+    for(int i = 0; i < n;i++){
+        int x;
+        printf("Masukkan jumlah koin di dalam peti  : \n");
+
+        char  *koin ;
+        koin = READINPUT() ;
+        x= StrToInt_input(koin , str_len(koin));
+        
+        BinTree t = NewTree(x, NULL, NULL);
+        ar[i] = t;
+    }
+    int j = 0;
+    while(jumlah < batas){
+        BinTree left, right;
+        BinTree t;
+        left = ar[j];
+        right = ar[j+1];
+        t = NewTree((ROOT(left) * ROOT(right)), left, right);
+        ar[n] = t;
+        j += 2;
+        n++;
+    }
+    BinTree p = ar[batas-1];
+    return p;
+
+}
+
+int hartakarun(){
+
+    int score;
+
+    //Pembuatan tree 
+    //Parent dari tree merupakan hasil perkalian kedua anak
+
+    printf("\n --------------------------------\n");
+    printf("\n|                                |\n");
+    printf("\n|  CREATE YOUR OWN TREASURE MAP  |\n");
+    printf("\n|                                |\n");
+    printf("\n --------------------------------\n");
+    printf("\n");
+
+    BinTree x = TimesTwo();
+    printf("\n Jalur untuk mendapatkan peti harta karun tujuan : \n");
+    printTree(x, 2);
+    printf("\n");
+
+    if(PathChosen(x)==peti(x)){
+        printf("\nJalur dengan harta karun terbanyak berhasil ditemukan.\n");
+        printf("Anda menang !\n");
+        score=100;
+    }
+    else{
+        printf("\nJalur yang anda pilih salah.\n");
+        printf("Anda kalah !\n");
+        score=0;
+    }
+
+    return score;
+
+}
---
+++ Tugas-Besar-1/src/GAME/hartakarun.h
@@ -0,0 +1,14 @@
+#ifndef HARTAKARUN_H
+#define HARTAKARUN_H
+
+#include <stdio.h>
+#include "bintree.h"
+#include "mesinkata.h"
+#include "mesinkarakter.h"
+
+int peti(BinTree root);
+int PathChosen(BinTree x);
+BinTree TimesTwo();
+int hartakarun();
+
+#endif
